1b92d3ebbc2a3179d5b0f77c6b8b5b09956b1831
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-03-17 16:38:51 +0300

    Specify 'code' type at class constructor

--- src/Exceptions/InvalidStatusCodeException.php
+++ src/Exceptions/InvalidStatusCodeException.php
@@ -15,7 +15,7 @@ final class InvalidStatusCodeException extends \InvalidArgumentException
      */
     private const EXCEPTION_PATTERN = 'Got undefined status code: %d';
 
-    public function __construct(int $invalidCode, $code = 0, Throwable $previous = null)
+    public function __construct(int $invalidCode, int $code = 0, Throwable $previous = null)
     {
         parent::__construct(sprintf(self::EXCEPTION_PATTERN, $invalidCode), $code, $previous);
     }
